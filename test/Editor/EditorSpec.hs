ff1e1445ba1a9835aec73c49ea17c3b286dea085
casselin <asselin.craig@gmail.com>
2020-11-18 14:42:01 -0500

    Delete redundant functions in Editor

--- test/Editor/EditorSpec.hs
+++ test/Editor/EditorSpec.hs
@@ -18,8 +18,6 @@ spec = do
     spec_appendLine
     spec_eNumLines
     spec_setMessageBar
-    spec_deleteLineAt
-    spec_deleteLine
 
 spec_eNumLines :: Spec
 spec_eNumLines = describe "eNumLines" $ do
@@ -49,36 +47,3 @@ spec_setMessageBar = describe "setMessageBar" $ do
             e = testEditor { eTime = t }
         setMessageBar e "Hello" `shouldBe`
             e { eMessageBar = newMessageBar "Hello" t }
-
-spec_deleteLineAt :: Spec
-spec_deleteLineAt = describe "deleteLineAt" $ do
-    it "deletes the EditorLine at the given position, and sets editor to dirty" $ do
-        let e = testEditor { eLines = Seq.fromList $
-                           [newELine "Hello", newELine "world"]
-                           }
-        deleteLineAt e 0 `shouldBe`
-            testEditor { eLines = Seq.singleton $ newELine "world"
-                       , eDirty = True
-                       }
-
-        deleteLineAt e 1 `shouldBe`
-            testEditor { eLines = Seq.singleton $ newELine "Hello"
-                       , eDirty = True
-                       }
-
-spec_deleteLine :: Spec
-spec_deleteLine = describe "deleteLine" $ do
-    it "deletes the EditorLine at the current y position" $ do
-        let e = testEditor { eLines = Seq.fromList $
-                           [newELine "Hello", newELine "world"]
-                           }
-        deleteLine e `shouldBe`
-            e { eLines = Seq.singleton $ newELine "world"
-              , eDirty = True
-              }
-
-        deleteLine (e { ecy = 1 }) `shouldBe`
-            e { eLines = Seq.singleton $ newELine "Hello"
-              , eDirty = True
-              , ecy = 1
-              }
